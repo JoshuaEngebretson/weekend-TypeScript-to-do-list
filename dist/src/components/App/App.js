ed1a3e671d1eb160732c93c706240a1ea5d24c36
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-07-02 14:20:51 -0500

    removed README.md from CRA, and replaced with README.old.md

--- dist/src/components/App/App.js
+++ dist/src/components/App/App.js
@@ -1,11 +1,26 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const jsx_runtime_1 = require("react/jsx-runtime");
+const react_1 = require("react");
+const axios_1 = __importDefault(require("axios"));
 require("./App.css");
-const ToDoListInputs_1 = require("../ToDoListInputs/ToDoListInputs");
 const ToDoListHeader_1 = require("../ToDoListHeader/ToDoListHeader");
 const RenderToDoList_1 = require("../RenderToDoList/RenderToDoList");
 function App() {
-    return ((0, jsx_runtime_1.jsxs)("div", { className: "App", children: [(0, jsx_runtime_1.jsx)(ToDoListHeader_1.ToDoListHeader, {}), (0, jsx_runtime_1.jsx)(ToDoListInputs_1.ToDoListInputs, {}), (0, jsx_runtime_1.jsx)(RenderToDoList_1.RenderToDoList, {})] }));
+    const [tasksCompleted, setTasksCompleted] = (0, react_1.useState)(0);
+    const [tasksTotal, setTasksTotal] = (0, react_1.useState)(0);
+    (0, react_1.useEffect)(() => {
+        calculateTasksCompleted();
+    }, []);
+    const calculateTasksCompleted = async () => {
+        const { data: tasks, } = await axios_1.default.get("/todo");
+        setTasksTotal(tasks.length);
+        const completedTasks = tasks.filter((item) => item.completed);
+        setTasksCompleted(completedTasks.length);
+    };
+    return ((0, jsx_runtime_1.jsxs)("div", { className: "App", children: [(0, jsx_runtime_1.jsx)(ToDoListHeader_1.ToDoListHeader, { tasksTotal: tasksTotal, tasksCompleted: tasksCompleted }), (0, jsx_runtime_1.jsx)(RenderToDoList_1.RenderToDoList, { calculateTasksCompleted: calculateTasksCompleted })] }));
 }
 exports.default = App;
