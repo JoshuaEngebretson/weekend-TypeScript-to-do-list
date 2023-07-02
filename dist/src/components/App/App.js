626adc7a9a36439352e8cfd4f2f3a24e378ff0c1
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-07-02 09:11:29 -0500

    Added mui into dependencies
    - Added MUI in order to utilize their pre-styled components
    	- Had to adjust the typescript version from 5.1.6 down to 4.9.5 for this to work
    
    - Started to build out my basic to-do-list form

--- dist/src/components/App/App.js
+++ dist/src/components/App/App.js
@@ -1,8 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const jsx_runtime_1 = require("react/jsx-runtime");
+const react_1 = require("react");
 require("./App.css");
+// const tasksCompleted =
 function App() {
-    return ((0, jsx_runtime_1.jsx)("div", { className: "App", children: (0, jsx_runtime_1.jsxs)("header", { className: "App-header", children: [(0, jsx_runtime_1.jsxs)("p", { children: ["Edit ", (0, jsx_runtime_1.jsx)("code", { children: "src/App.tsx" }), " and save to reload."] }), (0, jsx_runtime_1.jsx)("a", { className: "App-link", href: "https://reactjs.org", target: "_blank", rel: "noopener noreferrer", children: "Learn React" })] }) }));
+    const [taskInput, setTaskInput] = (0, react_1.useState)("");
+    const [assignedToInput, setAssignedToInput] = (0, react_1.useState)("");
+    const [taskNoteInput, setTaskNoteInput] = (0, react_1.useState)("");
+    return ((0, jsx_runtime_1.jsxs)("div", { className: "App", children: [(0, jsx_runtime_1.jsxs)("header", { className: "App-header", children: [(0, jsx_runtime_1.jsx)("h1", { children: "To-Do List" }), (0, jsx_runtime_1.jsx)("p", { children: "Tasks Completed: " })] }), (0, jsx_runtime_1.jsx)("main", { children: (0, jsx_runtime_1.jsx)("form", { children: (0, jsx_runtime_1.jsxs)("p", { children: [(0, jsx_runtime_1.jsx)("label", { htmlFor: "taskInput", children: "Enter Task" }), (0, jsx_runtime_1.jsx)("input", { id: "taskInput", type: "text", placeholder: "Required", value: taskInput }), (0, jsx_runtime_1.jsx)("br", {}), (0, jsx_runtime_1.jsx)("label", { htmlFor: "taskInput", children: "Assigned To" }), (0, jsx_runtime_1.jsx)("input", { id: "assignedToInput", type: "text", placeholder: "Required", value: assignedToInput }), (0, jsx_runtime_1.jsx)("br", {}), (0, jsx_runtime_1.jsx)("label", { htmlFor: "taskInput", children: "Task Note" }), (0, jsx_runtime_1.jsx)("input", { id: "taskNoteInput", type: "text", placeholder: "Optional", value: taskNoteInput })] }) }) })] }));
 }
 exports.default = App;
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
+// const tasksCompleted =
+
 function App() {
+	const [taskInput, setTaskInput] = useState("");
+	const [assignedToInput, setAssignedToInput] = useState("");
+	const [taskNoteInput, setTaskNoteInput] = useState("");
+
 	return (
 		<div className="App">
 			<header className="App-header">
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
+				<h1>To-Do List</h1>
+				<p>Tasks Completed: {}</p>
 			</header>
+
+			<main>
+				<form>
+					<p>
+						<label htmlFor="taskInput">Enter Task</label>
+						<input
+							id="taskInput"
+							type="text"
+							placeholder="Required"
+							value={taskInput}
+						/>
+						<br />
+						<label htmlFor="taskInput">Assigned To</label>
+						<input
+							id="assignedToInput"
+							type="text"
+							placeholder="Required"
+							value={assignedToInput}
+						/>
+						<br />
+						<label htmlFor="taskInput">Task Note</label>
+						<input
+							id="taskNoteInput"
+							type="text"
+							placeholder="Optional"
+							value={taskNoteInput}
+						/>
+					</p>
+				</form>
+			</main>
 		</div>
 	);
 }
