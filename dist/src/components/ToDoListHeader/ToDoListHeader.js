dcb9d79975a4b69a73ea80c75ab16925366fa7a7
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-07-02 10:38:14 -0500

    ToDoListHeader updates
    - Got the ToDoListHeader to have a dynamic tasks completed section.
    	- Now shows number completed out of total tasks

--- dist/src/components/ToDoListHeader/ToDoListHeader.js
+++ dist/src/components/ToDoListHeader/ToDoListHeader.js
@@ -1,9 +1,25 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ToDoListHeader = void 0;
 const jsx_runtime_1 = require("react/jsx-runtime");
+const react_1 = require("react");
+const axios_1 = __importDefault(require("axios"));
 const ToDoListHeader = () => {
-    // const tasksCompleted =
-    return ((0, jsx_runtime_1.jsxs)("header", { className: "App-header", children: [(0, jsx_runtime_1.jsx)("h1", { children: "To-Do List" }), (0, jsx_runtime_1.jsx)("p", { children: "Tasks Completed: " })] }));
+    const [tasksCompleted, setTasksCompleted] = (0, react_1.useState)(0);
+    const [tasksTotal, setTasksTotal] = (0, react_1.useState)(0);
+    const calculateTasksCompleted = async () => {
+        const { data: tasks, } = await axios_1.default.get("/todo");
+        console.log("tasks:", tasks);
+        setTasksTotal(tasks.length);
+        const completedTasks = tasks.filter((item) => item.completed);
+        setTasksCompleted(completedTasks.length);
+    };
+    (0, react_1.useEffect)(() => {
+        calculateTasksCompleted();
+    }, []);
+    return ((0, jsx_runtime_1.jsxs)("header", { className: "App-header", children: [(0, jsx_runtime_1.jsx)("h1", { children: "To-Do List" }), (0, jsx_runtime_1.jsxs)("p", { children: ["Tasks Completed: ", tasksCompleted, " / ", tasksTotal] })] }));
 };
 exports.ToDoListHeader = ToDoListHeader;
--- src/components/ToDoListHeader/ToDoListHeader.tsx
+++ src/components/ToDoListHeader/ToDoListHeader.tsx
@@ -1,11 +1,40 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export const ToDoListHeader = (): JSX.Element => {
-	// const tasksCompleted =
+	const [tasksCompleted, setTasksCompleted] = useState(0);
+	const [tasksTotal, setTasksTotal] = useState(0);
+
+	const calculateTasksCompleted = async () => {
+		const {
+			data: tasks,
+		}: {
+			data: [
+				{
+					assigned_to: string;
+					completed: boolean;
+					completed_date?: null | string;
+					created: string;
+					id: number;
+					task: string;
+					task_note?: string;
+				}
+			];
+		} = await axios.get("/todo");
+		console.log("tasks:", tasks);
+		setTasksTotal(tasks.length);
+		const completedTasks = tasks.filter((item) => item.completed);
+		setTasksCompleted(completedTasks.length);
+	};
+	useEffect(() => {
+		calculateTasksCompleted();
+	}, []);
 	return (
 		<header className="App-header">
 			<h1>To-Do List</h1>
-			<p>Tasks Completed: {}</p>
+			<p>
+				Tasks Completed: {tasksCompleted} / {tasksTotal}
+			</p>
 		</header>
 	);
 };
